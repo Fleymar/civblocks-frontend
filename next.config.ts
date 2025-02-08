51d6f8e9e65d2295ca319f96c2ff4b0cdf8b54a7
Fleymar <thibaudbultel@protonmail.com>
2025-02-08 23:58:31 +0100

    another try at the fix of paths in production

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
+console.log("NODE_ENV in next.config:", process.env.NODE_ENV);
 
 const nextConfig: NextConfig = {
   basePath: isProd ? '/nextjs-portfolio' : '',
   assetPrefix: isProd ? './' : '',
   output:'export',
   distDir: 'dist',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   }
