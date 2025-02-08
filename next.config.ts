05afac4ca85e67090184d5fdab8e9fcb20aa1343
Fleymar <thibaudbultel@protonmail.com>
2025-02-08 23:43:08 +0100

    fixing the prod Paths for gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
+  basePath: isProd ? '/nextjs-portfolio' : '',
+  assetPrefix: isProd ? '/nextjs-portfolio/' : '',
   output:'export',
   distDir: 'dist',
   images: {
