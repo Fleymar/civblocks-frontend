27d74c120c8df565ffa2f78c8d543599e4478a77
Fleymar <thibaudbultel@protonmail.com>
2025-02-09 00:16:16 +0100

    testing a random fix

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-console.log("NODE_ENV in next.config:", process.env.NODE_ENV);
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? '/nextjs-portfolio' : '',
-  assetPrefix: isProd ? '/nextjs-portfolio/' : undefined,
   output:'export',
   distDir: 'dist',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  }
 };
 
 export default nextConfig;
